2ce4918c81beac808fd53cb23a2a01c1132f5035
Sgw32 <sgw32@yandex.ru>
2018-08-03 17:01:07 +0300

    ver 2, smooth cutoff

--- main.c
+++ main.c
@@ -6,28 +6,61 @@
  */ 
 # define F_CPU 16000000UL
 
-#define TICKS_PER_MS (F_CPU / 1000);
+#define TICKS_PER_MS (F_CPU / 1000)
 
-#define RELAY_DELAY 100 // millis
-#define PWM_DELAY 700 // millis
-
-#define PWM_MAX (2500 * TICKS_PER_MS) / 1000 // ticks of TIMER1?
+#define RELAY_DELAY 100 // millis - задержка размыкания реле
+#define PWM_DELAY 2048 // millis - задержка до замыкания реле
+#define INTER_DELAY 3000
+#define PWM_MIN 16
+#define PWM_MAX 32 // ticks of TIMER2
+#define PWM_PER_TICK (PWM_MAX - PWM_MIN) / PWM_DELAY
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include "millis.h"
 #include "pwm.h"
 
 unsigned int a,b,c,high,period;
-int enabled;
-unsigned long relay_cnt;
 
-/*ISR( TIMER2_COMP_vect)
+const int timer2_prescaler = (1 << CS22) | (1 << CS21) | (1 << CS20);
+millis_t relay_done, pwm_done;
+ 
+inline void timer2_start()
 {
-	if (enabled)
-		PORTB=(PORTB&0b11000011)|(PIND&(0b00111100));
-}*/
+	TCCR2 = timer2_prescaler;
+}
 
+inline void timer2_stop()
+{
+	TCCR2 = 0;
+}
+
+ISR (TIMER2_COMP_vect)
+{
+	timer2_stop();
+	PORTC = PORTC ^ 2;	
+}
+
+ISR (INT0_vect)
+{
+	TCNT2 = 0;
+	timer2_start();
+	PORTC = PORTC ^ 1;
+}
+
+void inputinterrupt_init()
+{
+	MCUCR |= (1 << ISC01) | (1 << ISC00);    // set INT0 to trigger on ANY logic change
+	GICR |= (1 << INT0);      // Turns on INT0
+}
+
+void timer2_init()
+{
+	OCR2 = 0x7F;	
+	TIMSK |= (1 << OCIE2);
+	TCNT2 = 0xFF;
+}
 
 int main(void)
 {
@@ -38,31 +71,39 @@ int main(void)
 	DDRB = 0b11111110; //All output,expect last one
 
 	PORTC = 0x00;
-	DDRC = 0b11000011; //0b11110000, some input(PWM)
+	DDRC = 0b11111111; //0b11110000, some input(PWM)
 	
-	enabled=1;
 	ppm_input_init();
+	millis_init();
+	timer2_init();
+	inputinterrupt_init();
+	
 	ppm.ch[0]=0;
-	//relay_cnt=100000;
-	sei();
+	
+	
+	int pwm_limit = 0;
+	//const int pwm_lim_per_tick = PWM_MAX / PWM_DELAY;
+	
+	// t2 prescaler 1024
+	// ~16khz freq
+	// 1 tick = 1/16 ms
+	// 700ms = 700*16 ticks
+	//pwm_max_ticks = 700*16
+	// @7000: 2000 (2ms)
+	// 2ms = 32 ticks
+	// 
 	
 	int desired = 1;
-/*	int relay_cnt;
-	int pwm_cnt;*/
 	
-	int pwm_limit = PWM_MAX;
-	const int pwm_lim_per_tick = PWM_MAX / PWM_DELAY;
-	
-	int relay_done = 0;
-	int pwm_done = 0;
+	sei();	
 	
     while (1) 
     {
-		int now = millis();
+		millis_t now = millis();
 		
-		if(ppm.ch[0]>0)  	
+		if (ppm.ch[0]>0)  	
 		{
-			int rc_desired = (ppm.ch[0]>3043) ? 0 : 1;
+			int rc_desired = (ppm.ch[0]>3043) ? 0 : 1; //1 - проходит(при старте)
 			if (rc_desired != desired) {
 				desired = rc_desired;
 				if (desired)
@@ -73,47 +114,28 @@ int main(void)
 			ppm.ch[0]=0;
 		}
 		
-		if (desired) {
+		if (desired) { //при старте эта ветка
 			PORTD&=~(1<<PORTD7); // turn off relay immediately
 			if (now < relay_done)
 				pwm_limit = 0;
 			else if (now > relay_done + PWM_DELAY)
 				pwm_limit = PWM_MAX;
 			else
-				pwm_limit = (now - relay_done) * pwm_lim_per_tick;
+				pwm_limit = PWM_MIN + (now - relay_done) * PWM_PER_TICK;
 		}
 		else {			
-			pwm_limit = (now < pwm_done) ? (pwm_done - now) * pwm_lim_per_tick : 0;
-			if (now > pwm_done)/
+			pwm_limit = (now < pwm_done) ? ((pwm_done - now) * PWM_PER_TICK + PWM_MIN) : 0;
+			if (now > pwm_done + INTER_DELAY)
 				PORTD|=(1<<PORTD7);
 		}
 		
-		if (pwm_timer < pwm_limit) {
+		if (TCNT2 < pwm_limit) {
 			PORTB=(PORTB&0b11000011)|(PIND&(0b00111100)); //После выключения реле передаём данные с порта.			
-		}
-		
-		
-		/*if (relay_cnt==0)
-		{
-			//Подождали пока ESC выключится, и включили торможение через реле.
-			PORTD|=(1<<PORTD7);
-			relay_cnt++;
-		}
-		
-		if (enabled)
-		{
-			//Сначала реле выключается, потом включается ESC
-			if (relay_cnt < 434000)
-				relay_cnt++;
-			else				
-				PORTB=(PORTB&0b11000011)|(PIND&(0b00111100)); //После выключения реле передаём данные с порта.
-		}
+		}	
 		else
 		{
-			//Сначала ESC выключается, потом реле включается.
-			PORTB=(PORTB&0b11000011)|(PINC&(0b00111100));
-			relay_cnt--;
-		}*/		
+			PORTB&=0b11000011;
+		}
     }
 }
 
